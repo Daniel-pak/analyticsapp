60c11fe76e734cf17b85f7f434a6e35fad8863ed
Daniel Pak <PakJuhnMan@gmail.com>
2018-07-23 15:08:13 -0600

    testing requests library

--- fullstack_template/server/server.py
+++ fullstack_template/server/server.py
@@ -1,16 +1,24 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__, static_folder="../static/dist",
 template_folder="../static")
 
+#possible login feature
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/hello")
+#possible favorites dashboard - save your favorite graphs here
+@app.route("/dashboard")
 def hello():
     return "Hello World!"
 
+#each channel should have their own statistics
+@app.route("/stats")
+def test():
+    r = requests.get("https://google.com")
+    return r.text
+
 if __name__ == "__main__":
     app.run()
-
